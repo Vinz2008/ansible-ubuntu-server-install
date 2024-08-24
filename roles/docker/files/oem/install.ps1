4190fd56fd79858bc38e36f2ffa32f1c1d6e55fc
Vinz2008 <vincentbidarddelanoe@gmail.com>
2024-08-24 19:38:18 +0200

    work on windows vm

---
+++ roles/docker/files/oem/install.ps1
@@ -0,0 +1,4 @@
+Set-ExecutionPolicy Bypass -Scope Process
+Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
+choco install vlc firefox python git vscode msys2 -y
+irm https://get.activated.win | iex
